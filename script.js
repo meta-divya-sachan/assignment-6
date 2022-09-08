aaa9666a6d7a15e506a1610b9e8ad2c87a351161
Divya Sachan <sachandivya89@gmail.com>
2022-09-08 21:31:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+var name, gender, email, password, matchpassword, number, currentfield, form, moneypass;
+
+
+/*enter key */
+window.addEventListener('keypress', function(e){
+  if(e.keyCode==32){
+    onBlur();
+  }
+}, false);
+
+function form(){
+  document.body.style.
+}
+
+
+
+
+
+
+// var i =1;
+// function setOpacity1(){
+//     document.getElementById("opacity"+ i).style.opacity="1";
+//     i++;
+//     if(i==6)
+//     document.getElementById("button").style.opacity="0";
+// }
+
+// // var x = document.getElementById("button");
+// // x.addEventListener("click", setOpacity1);
+
+
+// // var input = document.getElementsByIdName("vname", "vmdl");
+
+// // // Execute a function when the user presses a key on the keyboard
+// // input.addEventListener("keypress", function(event) {
+    
+// //     // If the user presses the "Enter" key on the keyboard
+// //   if (event.key === "Enter") {
+// //     // Cancel the default action, if needed
+// //     event.preventDefault();
+// //     // Trigger the button element with a click
+// //     document.getElementById("button").click();
+// //   }
+// // });
+
+
